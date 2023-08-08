5f21777d80b31196158d6d6e5af531efa54c2e72
OV2XMP VM <ov2xmp@trsc.net>
2023-08-08 18:10:35 +0000

    fixed uploading, security scan for filename

--- http-server.py
+++ http-server.py
@@ -5,17 +5,30 @@ logging.basicConfig(level=logging.INFO)
 from sanic_httpauth import HTTPBasicAuth
 import hashlib
 import json
+from werkzeug.utils import secure_filename
+
 
 app = Sanic(__name__)
 auth = HTTPBasicAuth()
 
 app.config.FALLBACK_ERROR_FORMAT = "json"
-app.static('/files/', '/files/', directory_view=True)
+#app.static('/files/', '/files/', directory_view=True)
 
 f = open('config.json')
 config = json.load(f)
 username = config['username']
 password = config['password']
+
+
+async def write_file(path, body):
+    async with aiofiles.open(path, 'wb') as f:
+        await f.write(body)
+
+
+def valid_file_size(file_body):
+    if len(file_body) < 10485760:
+        return True
+    return False
 
 
 def hash_password(salt, password):
@@ -39,8 +52,12 @@ def verify_password(username, password):
 @app.post('/')
 @auth.login_required
 async def upload_file(request):
-    async with aiofiles.open("/files/" + request.files["file"][0].name, 'wb') as f:
-        await f.write(request.files["file"][0].body)
-    await f.close()
+    upload_file = request.files.get('file')
+
+    if upload_file is not None:
+        filename = secure_filename(upload_file.name)
+        file_path = '/files/' + filename
+        if valid_file_size(upload_file.body):
+            await write_file(file_path, upload_file.body)
 
     return response.json(True)
